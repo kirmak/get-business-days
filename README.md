7ef457a5d1851ee635578e0588f5d645d0204846
Kirill Lukianenko <kirmak@yandex.ru>
2016-11-23 14:19:41 +0100

    inclusive mode & tests

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # get-business-days
 Calculate business days between two dates.
+
+## Usage:
+```javascript
+getBusinessDays(fromDate, tillDate, options)
+```
+
+## Parameters
+
+*fromDate*
+Date object or any object which could be passed to Date contructor (number of milliseconds or valid date string).
+
+*tillDate*
+Date object or any object which could be passed to Date contructor.
+
+*options*
+An object containing calculation options:
+*options.holidays* is array of Date objects or any object which could be passed to Date constructor. Public holiday dates, which will be removed from resulting count.
+*options.exclusive* a boolean value to specify calculation method. By default, between today and tomorrow are two business days. If *exclusive* option is set, tomorrow will be excluded, and return value will be 1.
+
+## Return value
+A Number, representing count of business days between *fromDate* and *tillDate* with respect of specified *options.holidays* and calculation method.
--- script/get-business-days.js
+++ script/get-business-days.js
@@ -9,10 +9,11 @@ var getBusinessDays = (function() {
     };
 
     var oneDay = 86400000; //milliseconds    
-    return (function(firstDate, secondDate, holidays){
-        if (!isArray(holidays))
-            holidays = [];
-            
+
+    return (function(firstDate, secondDate, options){
+    
+        options = options || {holidays: [], exclusive: true};
+
         if (!isDate(firstDate))
             firstDate = new Date(firstDate);
             
@@ -23,29 +24,46 @@ var getBusinessDays = (function() {
         var endDate = new Date(Math.floor(Math.max(firstDate, secondDate) / oneDay) * oneDay);
 
         var days = (endDate - startDate) / oneDay;
+        if (!options.exclusive)
+            days++;
+            
         days -= Math.floor(days / 7) * 2;
         
         var firstDay = startDate.getDay();
         var lastDay = endDate.getDay();
         
-        if (!(firstDay > 0 && firstDay < lastDay && (lastDay > 0 && lastDay < 6))) {
-            
-            if (firstDay == 0)
-                days--;
-            
-            if (lastDay == 0)
-                days--;
-            else if (firstDay > lastDay)
-                days -= Math.min(7 - (lastDay - firstDay), 2);
+        if (options.exclusive) {
+            if (firstDay != lastDay) {
+                if (firstDay < lastDay) {
+                    if (firstDay == 0)
+                        days--;
+                } else {
+                    if (lastDay != 0)
+                        days--;
+                    days--;
+                }                
+            }
+        } else {
+            if (firstDay != ((lastDay + 1) % 7)) { // If non-whole week, calculate the rest.
+                if (firstDay <= lastDay) { // The rest doesn't cross weekend (SAT-SUN)
+                    if (firstDay == 0 || lastDay == 6) // Only one day could be SAT or SUN. If first = SUN and last = SAT it won't go here due to first "if" (weekend is already counted).
+                        days--;
+                } else {
+                    days -= 2; // In this case we should always remove whole weekend
+                }
+            }
         }
+        
+        if (!isArray(options.holidays))
+            options.holidays = [];
 
-        for (var i = 0; i < holidays.length; i++) {
-            var holiday = holidays[i];
+        for (var i = 0; i < options.holidays.length; i++) {
+            var holiday = options.holidays[i];
             holiday = new Date(((isDate(holiday) ? holiday : new Date(holiday)) / oneDay) * oneDay);
             if (holiday >= startDate && holiday <= endDate && 0 < holiday.getDay() && holiday.getDay() < 6)
                 days--;
         }
         
         return days;
-    });    
+    });
 }());
